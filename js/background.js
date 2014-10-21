d8be31ee3d2728aeab4f6eb24f4565ab45d19cba
Earl Sargent <esargent01@gmail.com>
2014-10-21 13:58:33 -0500

    Small changes to the PPVN
    I am making small cosmetic changes to the Codrops code to suit my
    desires.
    
    First commit: Pictures of topic appear when hovering over the menu
    item. Currently working on small fade-in effect for images, then small
    background video preview in place of image. Images are taken from web
    instead of image folder.

---
+++ js/background.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+  $(".icon-home").hover(function(){
+    $(".perspective.modalview ").css("background","url(http://techbeasts.com/wp-content/uploads/2014/04/Wallpaper-HD.jpg) no-repeat").css("background-size","cover");/*this is the background image*/
+    },function(){
+    $(".perspective.modalview ").css("background","none");/*this chages the background back to gray :D */
+  });
+  $(".icon-news").hover(function(){
+    $(".perspective.modalview ").css("background","url(http://images6.fanpop.com/image/photos/34800000/Nature-Wallpaper-daydreaming-34811106-1024-768.jpg) no-repeat").css("background-size","cover");
+    },function(){
+    $(".perspective.modalview ").css("background","none");
+  });
+  $(".icon-image").hover(function(){
+    $(".perspective.modalview ").css("background","url(http://www.hdwallpapersfullhd.net/wp-content/uploads/2014/05/Wallpaper-Wallpaper-Lampadas-Wallpapers.jpg) no-repeat").css("background-size","cover");
+    },function(){
+    $(".perspective.modalview ").css("background","none");
+  });
+  $(".icon-upload").hover(function(){
+    $(".perspective.modalview ").css("background","url(http://www.smitwallpaper.com/wallpaper/original/Technology-Wallpaper-6_3.jpg) no-repeat").css("background-size","cover");
+    },function(){
+    $(".perspective.modalview ").css("background","none");
+  });
+  $(".icon-star").hover(function(){
+    $(".perspective.modalview ").css("background","url(http://www.smitwallpaper.com/wallpaper/original/chinese_garden_wallpaper-1366x768.jpg) no-repeat").css("background-size","cover");
+    },function(){
+    $(".perspective.modalview ").css("background","none");
+  });
+  $(".icon-mail").hover(function(){
+    $(".perspective.modalview ").css("background","url(http://www.smitwallpaper.com/wallpaper/original/Madagascar-Wallpaper-HD.jpg) no-repeat").css("background-size","cover");
+    },function(){
+    $(".perspective.modalview ").css("background","none");
+  });
+  $(".icon-lock").hover(function(){
+    $(".perspective.modalview ").css("background","url(http://techbeasts.com/wp-content/uploads/2014/04/Wallpaper-HD.jpg) no-repeat").css("background-size","cover");
+    },function(){
+    $(".perspective.modalview ").css("background","none");
+  });
+});
